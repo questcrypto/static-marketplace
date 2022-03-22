068c0eb846c9a319f53e13a2e0f10ffba2021864
Matie J. Natov <matiedesign@gmail.com>
2022-03-22 14:32:54 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+    $('div.hidden').fadeIn("slow").removeClass('hidden');
+
+      
+
+    $('.slider-for').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      accessibility: true,
+      arrows:false
+    });
